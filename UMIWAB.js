7ffec0a54ec04a523efef79e567585f3a6f39edd
DIFA <42770277+green404@users.noreply.github.com>
2022-04-15 14:26:46 +0700

    Add files via upload

---
+++ UMIWAB.js
@@ -0,0 +1,18 @@
+{
+	"version": "14.0",
+	"download": "https://www.difarosyidin.com/2022/03/umi-wabusiness-v120.html",
+	"update": [
+		{
+			"languaje": "en",
+			"changelog": "https://raw.githubusercontent.com/green404/UMI/main/changelog-B-en.html"
+		},
+		{
+			"languaje": "es",
+			"changelog": "https://raw.githubusercontent.com/green404/UMI/main/changelog-B-es.html"
+		},
+		{
+			"languaje": "in",
+			"changelog": "https://raw.githubusercontent.com/green404/UMI/main/changelog-B-in.html"
+		}
+	]
+}
